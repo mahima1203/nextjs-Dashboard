36639a2e83cefd0e41332969d9bb4db3abca8494
unknown <mahimapathania1203@gmail.com>
2026-01-17 17:39:27 +0530

    Added error handling and not found files.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,47 +6,53 @@ import { redirect } from 'next/navigation';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const formSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['paid', 'pending', 'overdue']),
-    date: z.string(),
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['paid', 'pending', 'overdue']),
+  date: z.string(),
 });
-const CreateInvoice=formSchema.omit({id: true,date: true});
+const CreateInvoice = formSchema.omit({ id: true, date: true });
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-    const amountInCents = Math.round(amount * 100);
-    const date = new Date().toISOString().split('T')[0];
-    console.log('Creating invoice with data:', { customerId, amount, status });
-    console.log(typeof amount);
-    await sql `INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = Math.round(amount * 100);
+  const date = new Date().toISOString().split('T')[0];
+  console.log('Creating invoice with data:', { customerId, amount, status });
+  console.log(typeof amount);
+  try {
+    await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+  } catch (error) {
+    console.error(error);
+    throw new Error('Database Error: Failed to Create Invoice.');
+  };
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 // Use Zod to update the expected types
 const UpdateInvoice = formSchema.omit({ id: true, date: true });
- 
- 
+
+
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
   const amountInCents = amount * 100;
- 
+
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
